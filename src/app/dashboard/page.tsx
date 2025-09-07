8e3a652c31b6b1e3768aab96b786aacddbbcbdba
Ari <arilife.com@gmail.com>
2025-09-07 22:16:57 +0900

    Set up shadcn/ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,18 +18,22 @@ const DashBoard = () => {
   }, []);
 
   return (
-    <ul>
-      <h2 className="text-2xl font-bold mb-4">ユーザーの食事記録</h2>
-      {meals.map((meal) => (
-        <li key={meal.id}>
-          <div>
-            <p>食事名：{meal.foodName}</p>
-            <p>摂取量：{meal.gram}グラム</p>
-            <p>摂取カロリー{meal.kcal}カロリー</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-6 pb-20 sm:p-20">
+      <main className="flex flex-col gap-[32px] w-full max-w-md row-start-2 items-center">
+        <ul>
+          <h2 className="text-2xl font-bold mb-4">ユーザーの食事記録</h2>
+          {meals.map((meal) => (
+            <li key={meal.id}>
+              <div>
+                <p>食事名：{meal.foodName}</p>
+                <p>摂取量：{meal.gram}グラム</p>
+                <p>摂取カロリー{meal.kcal}カロリー</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 };
 
