e9c35bb0419b1f86ba68a142dfee1a0dd6bc6497
Ari <arilife.com@gmail.com>
2025-09-08 21:33:11 +0900

    Setup Supabase Auth and middleware

---
+++ src/utils/supabase/middleware.ts
@@ -0,0 +1,57 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+
+  // Get session
+  await supabase.auth.getClaims();
+
+  // Get user info
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const publicPaths = ["/login", "/auth", "/signup"];
+
+  if (
+    request.nextUrl.pathname === "/" ||
+    publicPaths.some((path) => request.nextUrl.pathname.startsWith(path))
+  ) {
+    return supabaseResponse;
+  }
+
+  // If no user info redirect to login page
+  if (!user) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  return supabaseResponse;
+}
