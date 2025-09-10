74ba4394fb75215a15229d9e59833641b01d126c
Ari <arilife.com@gmail.com>
2025-09-10 22:34:40 +0900

    Create login action

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -1,7 +1,30 @@
 "use server";
 
+import { loginFormInput } from "@/app/auth/_components/_types";
 import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+
+export async function login(formData: loginFormInput) {
+  const supabase = await createClient();
+  const email = formData.email;
+  const password = formData.password;
+
+  if (!email || !password) return;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email: email as string,
+    password: password as string,
+  });
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  revalidatePath("/");
+  redirect("/");
+}
 
 export async function loginGoogle() {
   const supabase = await createClient();
