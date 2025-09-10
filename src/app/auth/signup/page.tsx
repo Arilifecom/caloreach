dfc4e89cf6da2da325757597d672398b4ab1c8b9
Ari <arilife.com@gmail.com>
2025-09-10 22:32:57 +0900

    Create UI for /signin page

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+import { SignupForm } from "@/app/auth/_components";
+
+export default function SignUp() {
+  return (
+    <>
+      <SignupForm />
+    </>
+  );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./Editbox";
+export * from "./FormHeader";
+export * from "./ButtonWithIconLabel";
+export * from "./List";
+export * from "./PageHeader";
+export * from "./VerticalLineWithText";
+export * from "./VerticalLine";
