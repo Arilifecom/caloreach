eee0df689d21f347995ad727c51f1d94a8ae1d3e
Ari <arilife.com@gmail.com>
2025-09-12 15:13:47 +0900

    Create password reset action

---
+++ src/actions/auth/ressetPassWord.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { resetPassWordFormInput } from "@/app/auth/_components/_types";
+import { createClient } from "@/utils/supabase/server";
+
+export async function resetPassWord(formData: resetPassWordFormInput) {
+  const supabase = await createClient();
+  const email = formData.email;
+
+  if (!email) return;
+
+  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: "http://localhost:3000/auth/reset-password/",
+  });
+
+  if (error) {
+    console.error(error);
+    throw new Error("メール送信に失敗しました");
+  }
+
+  return;
+}
