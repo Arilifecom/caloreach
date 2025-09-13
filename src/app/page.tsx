f89e2665753d7f041dfc164cd0540fcab98f3bd2
Ari <arilife.com@gmail.com>
2025-09-14 00:57:56 +0900

    Remove logout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-import { logout } from "@/actions/auth";
 import { getMealRecordAction } from "@/actions/mealRecords/getList";
-import { LogoutButton } from "@/app/auth/_components";
+import { Button } from "@/components/ui";
 import { SelectMealRecord } from "@/db/schema";
+import { createClient } from "@/utils/supabase/client";
+import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [meals, setMeals] = useState<SelectMealRecord[]>([]);
+  const router = useRouter();
 
   const userId = "ded6e710-8c55-45b8-a7ed-8cd57b7d06ff";
 
   const handleLogOut = async () => {
-    try {
-      await logout();
-    } catch (error) {
-      console.error(error);
+    const supabase = await createClient();
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("サインアウトに失敗しました", error);
+      return;
     }
+
+    return router.push("/auth/login");
   };
 
   useEffect(() => {
@@ -30,7 +36,9 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen text-sm p-6 pb-20 sm:p-20">
       <main className="flex flex-col gap-[32px] w-full max-w-md row-start-2 items-center">
-        <LogoutButton handleLogout={handleLogOut} />
+        <Button onClick={handleLogOut}>
+          <LogOut />
+        </Button>
         <ul>
           <h2 className="text-2xl font-bold mb-4">ユーザーの食事記録</h2>
           {meals.map((meal) => (
