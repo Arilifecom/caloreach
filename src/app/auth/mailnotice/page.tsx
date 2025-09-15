f1264643b73233d6884a37751beb2028fc69d746
Ari <arilife.com@gmail.com>
2025-09-15 15:33:47 +0900

    Change method for email sent message

--- src/app/auth/mailnotice/page.tsx
+++ src/app/auth/mailnotice/page.tsx
@@ -1,39 +1,15 @@
-import { EmailSentNotice, LoginButton } from "@/app/auth/_components";
-import { use } from "react";
+import { EmailSentNotice } from "@/app/auth/_components";
+interface MailNoticePageProps {
+  searchParams: {
+    type: "verify" | "reset" | "reset-success";
+  };
+}
 
-type MailNoticePageProps = {
-  searchParams: { type?: string };
-};
+export default async function MailNoticePage({
+  searchParams,
+}: MailNoticePageProps) {
+  const params = await searchParams;
+  const type = params.type;
 
-export default function MailNoticePage({ searchParams }: MailNoticePageProps) {
-  const params = use(Promise.resolve(searchParams));
-
-  switch (params.type) {
-    case "verify":
-    default:
-      return (
-        <EmailSentNotice
-          title="Check your Email"
-          description="確認メールを送信しました"
-          body="メール内のリンクをクリックして登録を完了してください。"
-        />
-      );
-    case "reset":
-      return (
-        <EmailSentNotice
-          title="Check your Email"
-          description="パスワードリセットメールを送信しました"
-          body="メール内のリンクをクリックして、新しいパスワードを設定してください。"
-        />
-      );
-    case "reset-success":
-      return (
-        <EmailSentNotice
-          title="Password Updated"
-          description="パスワードの更新が完了しました"
-          body="新しいパスワードでログインしてください。"
-          actionButton={<LoginButton />}
-        />
-      );
-  }
+  return <EmailSentNotice type={type} />;
 }
