cbc454b9999910ac282c9a0cc9f0b59c1a192ec1
Ari <arilife.com@gmail.com>
2025-09-16 11:21:36 +0900

    Fix middleware delete getUser() function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
 import { getMealRecordAction } from "@/actions/mealRecords/getList";
 import { LogoutButton } from "@/components";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-const userId = "ded6e710-8c55-45b8-a7ed-8cd57b7d06ff";
+//Check user session
+const authenticated = async () => {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getClaims();
+
+  if (error || !data) {
+    console.error("Error fetching Claims:", error || "no data");
+
+    redirect("/auth/login");
+  }
+};
+
+//Get user ID
+const getUserId = async () => {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data) {
+    console.error("Error fetching Claims:", error || "no data");
+    redirect("/auth/login");
+  }
+
+  return data.user.id;
+};
 
 export default async function Home() {
+  await authenticated();
+  const userId = await getUserId();
   const meals = await getMealRecordAction(userId);
 
   return (
@@ -12,15 +39,21 @@ export default async function Home() {
         <LogoutButton />
         <ul>
           <h2 className="text-2xl font-bold mb-4">ユーザーの食事記録</h2>
-          {meals.map((meal) => (
-            <li key={meal.id}>
-              <div>
-                <p>食事名：{meal.foodName}</p>
-                <p>摂取量：{meal.gram}グラム</p>
-                <p>摂取カロリー{meal.kcal}カロリー</p>
-              </div>
-            </li>
-          ))}
+          {meals.length > 0 ? (
+            meals.map((meal) => (
+              <li key={meal.id}>
+                <div>
+                  <p>食事名：{meal.foodName}</p>
+                  <p>摂取量：{meal.gram}グラム</p>
+                  <p>摂取カロリー{meal.kcal}カロリー</p>
+                </div>
+              </li>
+            ))
+          ) : (
+            <p className="font-medium text-center">
+              今日の食事記録はありません
+            </p>
+          )}
         </ul>
       </main>
     </div>
