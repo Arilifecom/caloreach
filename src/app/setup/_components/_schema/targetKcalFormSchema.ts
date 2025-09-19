ac714fd4a738d9b42b0b3faaa3ab14966cd97514
Ari <arilife.com@gmail.com>
2025-09-19 14:11:41 +0900

    Create schema for TargetKcalForm

---
+++ src/app/setup/_components/_schema/targetKcalFormSchema.ts
@@ -0,0 +1,21 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const toHalfWidth = (val: string) =>
+  val.replace(/[０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0));
+
+export const TargetKcalInputSchema = z.object({
+  targetKcal: z
+    .string()
+    .transform(toHalfWidth)
+    .transform((val) => Number(val))
+    .refine((val) => val > 100, {
+      message: "100以上の値を入力してください",
+    }),
+});
+
+export const TargetKcalInputInputResolver = zodResolver(TargetKcalInputSchema);
+export type TargetKcalInputSchemaInput = z.input<typeof TargetKcalInputSchema>;
+export type TargetKcalInputSchemaOutput = z.output<
+  typeof TargetKcalInputSchema
+>;
