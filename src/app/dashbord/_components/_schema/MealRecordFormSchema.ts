46ba0583ff417a5068fe3755c518621a4c6e08de
Ari <arilife.com@gmail.com>
2025-09-29 12:16:17 +0900

    Create mealRecord schema

---
+++ src/app/dashbord/_components/_schema/MealRecordFormSchema.ts
@@ -0,0 +1,38 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const toHalfWidth = (val: string) =>
+  val.replace(/[０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0));
+
+const mealRecordInputSchema = z
+  .object({
+    date: z.string().min(1, "日付を入力してください"),
+    time: z.string().min(1, "時間を入力してください"),
+    foodName: z.string().min(1, "食事名を入力してください"),
+    gram: z
+      .string()
+      .transform(toHalfWidth)
+      .transform((val) => Number(val))
+      .refine((val) => val > 1, {
+        message: "食事量を入力してください",
+      }),
+    kcal: z
+      .string()
+      .transform(toHalfWidth)
+      .transform((val) => Number(val))
+      .refine((val) => val > 1, {
+        message: "カロリー量を入力してください",
+      }),
+  })
+  .transform((data) => ({
+    foodName: data.foodName,
+    gram: data.gram,
+    kcal: data.kcal,
+    eatenAt: new Date(`${data.date}T${data.time}:00`),
+  }));
+
+export const mealRecordSchemaResolver = zodResolver(mealRecordInputSchema);
+export type mealRecordInputSchemaInput = z.input<typeof mealRecordInputSchema>;
+export type mealRecordInputSchemaOutput = z.output<
+  typeof mealRecordInputSchema
+>;
