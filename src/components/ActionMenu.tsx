b62747950cb037b01a986f1eb4760504fab4a894
Ari <arilife.com@gmail.com>
2025-10-13 23:09:37 +0900

    Refactor styles

--- src/components/ActionMenu.tsx
+++ src/components/ActionMenu.tsx
@@ -43,38 +43,37 @@ const Component = ({ mealRecord }: ActionMenuProps) => {
       </button>
 
       {isOptionOpen && (
-        <div className="fixed inset-0 z-10" onClick={handleOptionWindow} />
-      )}
-
-      {isOptionOpen && (
-        <div className="absolute border-1 rounded-lg  right-0 -top-[190px] flex flex-col gap-2 z-20 items-center justify-center bg-muted p-6 transition-opacity duration-300">
-          <button
-            onClick={handleInputFormWindow}
-            className="flex items-center bg-foreground w-[44px] h-[44px] p-3 rounded-lg"
-          >
-            <Pencil className="text-popover" />
-          </button>
-          <MealRecordForm
-            mode="edit"
-            editItem={mealRecord}
-            userId={mealRecord.id}
-            isFormOpen={isFormOpen}
-            handleInputFormWindow={handleInputFormWindow}
-            handleCloseAllWindows={handleCloseAllWindows}
-          />
-          <button
-            onClick={() => handleDelete(mealRecord.id)}
-            className="bg-muted border-2 flex items-center h-[44px] p-3 w-[44px] rounded-lg"
-          >
-            <Trash2 />
-          </button>
-          <button
-            onClick={handleOptionWindow}
-            className="bg-muted border-2 flex items-center h-[44px] p-3 w-[44px] rounded-lg"
-          >
-            <X />
-          </button>
-        </div>
+        <>
+          <div className="fixed inset-0 z-10" onClick={handleOptionWindow} />
+          <div className="absolute border-1 rounded-lg  right-0 -top-[190px] flex flex-col gap-2 z-20 items-center justify-center bg-muted p-6 transition-opacity duration-300">
+            <button
+              onClick={handleInputFormWindow}
+              className="flex items-center bg-foreground w-[44px] h-[44px] p-3 rounded-lg"
+            >
+              <Pencil className="text-popover" />
+            </button>
+            <MealRecordForm
+              mode="edit"
+              editItem={mealRecord}
+              userId={mealRecord.id}
+              isFormOpen={isFormOpen}
+              handleInputFormWindow={handleInputFormWindow}
+              handleCloseAllWindows={handleCloseAllWindows}
+            />
+            <button
+              onClick={() => handleDelete(mealRecord.id)}
+              className="bg-muted border-2 flex items-center h-[44px] p-3 w-[44px] rounded-lg"
+            >
+              <Trash2 />
+            </button>
+            <button
+              onClick={handleOptionWindow}
+              className="bg-muted border-2 flex items-center h-[44px] p-3 w-[44px] rounded-lg"
+            >
+              <X />
+            </button>
+          </div>
+        </>
       )}
     </>
   );
