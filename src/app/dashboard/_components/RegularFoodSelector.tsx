f96373b02f72b3d4c46f28d7ea85243e663a6646
Ari <arilife.com@gmail.com>
2025-10-22 14:31:13 +0900

    Error Handling

---
+++ src/app/dashboard/_components/RegularFoodSelector.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { ErrorFallback, RegularFoodLsits } from "@/app/dashboard/_components/";
+import { FormHeader } from "@/components";
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { memo } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+type RegularFoodSelectorProps = {
+  userId: string;
+  isRegularOpen: boolean;
+  handleRegularMealsWindow: () => void;
+  handleCloseAllWindows: () => void;
+};
+
+const Component = ({
+  userId,
+  isRegularOpen,
+  handleCloseAllWindows,
+  handleRegularMealsWindow,
+}: RegularFoodSelectorProps) => {
+  return (
+    <>
+      {isRegularOpen && (
+        <div
+          className="fixed inset-0 bg-foreground/20 z-10"
+          onClick={handleRegularMealsWindow}
+        />
+      )}
+
+      <div
+        className={`fixed bottom-0 bg-background w-full rounded-tr-lg rounded-tl-lg max-w-lg z-30 overflow-hidden transition-all duration-300 ease-in-out
+      ${
+        isRegularOpen
+          ? "h-[80vh] pt-8 border-2 border-foreground"
+          : "h-0 pt-0 border-0"
+      }`}
+      >
+        <FormHeader
+          title="レギュラーフードから食事を記録"
+          description="食事を追加してください"
+          handleClose={handleCloseAllWindows}
+        />
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary onReset={reset} FallbackComponent={ErrorFallback}>
+              <RegularFoodLsits
+                userId={userId}
+                isRegularOpen={isRegularOpen}
+                handleCloseAllWindows={handleCloseAllWindows}
+              />
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
+      </div>
+    </>
+  );
+};
+
+const RegularFoodSelector = memo(Component);
+export { RegularFoodSelector };
