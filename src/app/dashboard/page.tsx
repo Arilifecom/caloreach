bd11e56839a1f41bf9218daa567e532a1572fda5
Ari <arilife.com@gmail.com>
2025-10-22 15:54:36 +0900

    Fix Toast

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { ErrorFallback, MealRecordSection } from "@/app/dashboard/_components";
 import { LogoutButton, PageHeader } from "@/components";
+import { Toaster } from "@/components/ui";
 import { fetchUserDailyMealRecords } from "@/utils/api/mealRecords";
 import { checkAuth, getUser } from "@/utils/auth";
 import { formatDateWithDay, getToday } from "@/utils/format";
@@ -25,12 +26,12 @@ export default async function Dashboard() {
   return (
     <div className="relative font-sans grid grid-rows-[20px_1fr_20px] mx-auto justify-items-center min-h-screen max-w-md text-sm p-6 pb-20 sm:p-20">
       <main className="flex flex-col gap-[32px] w-full row-start-2 items-center">
+        <Toaster />
         <LogoutButton />
         <PageHeader
           title={formatDateWithDay(displayDate)}
           description="目標達成までがんばろう！"
         />
-
         <HydrationBoundary state={dehydratedState}>
           <ErrorBoundary FallbackComponent={ErrorFallback}>
             <MealRecordSection userId={userId} />
