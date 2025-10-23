082d9a0bb3b08c3c86a2c131cf4fb66f3c60aa7d
Ari <arilife.com@gmail.com>
2025-10-23 14:34:09 +0900

    Create Form schema

---
+++ src/app/dashboard/regular-foods/_components/_schema/RegularFoodFormSchema.ts
@@ -0,0 +1,35 @@
+import { toHalfWidth } from "@/utils/format";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const RegularFoodFormSchema = z
+  .object({
+    foodName: z.string().min(1, "食事名を入力してください"),
+    gram: z
+      .string()
+      .transform(toHalfWidth)
+      .transform((val) => Number(val))
+      .refine((val) => val > 1, {
+        message: "食事量を入力してください",
+      }),
+    kcal: z
+      .string()
+      .transform(toHalfWidth)
+      .transform((val) => Number(val))
+      .refine((val) => val > 1, {
+        message: "カロリー量を入力してください",
+      }),
+  })
+  .transform((data) => ({
+    foodName: data.foodName,
+    gram: data.gram,
+    kcal: data.kcal,
+  }));
+
+export const RegularFoodFormSchemaResolver = zodResolver(RegularFoodFormSchema);
+export type RegularFoodFormInputSchemaInput = z.input<
+  typeof RegularFoodFormSchema
+>;
+export type RegularFoodFormInputSchemaOutput = z.output<
+  typeof RegularFoodFormSchema
+>;
