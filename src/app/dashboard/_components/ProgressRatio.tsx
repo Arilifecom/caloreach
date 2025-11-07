b4eb1cbb8e3e86259e4f1310e0914cf037b26853
Ari <arilife.com@gmail.com>
2025-11-07 16:23:30 +0900

    Create Progress percentage UI

---
+++ src/app/dashboard/_components/ProgressRatio.tsx
@@ -0,0 +1,44 @@
+import { FlagIcon } from "@/components/icons";
+import { CardContent, CardWithShadow } from "@/components/ui";
+import { cn } from "@/lib/utils";
+import { memo } from "react";
+
+type ProgressRatioProps = {
+  progressValue: number | null;
+  isLoading: boolean;
+  className?: string;
+  isError: boolean;
+};
+
+const Component = ({
+  progressValue,
+  isLoading,
+  className,
+  isError,
+}: ProgressRatioProps) => {
+  return (
+    <CardWithShadow className={cn("py-4", className)}>
+      <CardContent className="px-4">
+        <div className="flex justify-left gap-2">
+          <FlagIcon className="w-8" />
+          <div className="flex flex-col">
+            <span className="text-xs font-medium -mb-1">達成率</span>
+            {isLoading ? (
+              <span className="text-xs text-gray-500 pt-2">取得中...</span>
+            ) : isError ? (
+              <p className="text-xl font-black">--</p>
+            ) : (
+              <p className="text-xl font-black">
+                {progressValue}
+                <span className="text-sm ml-1">%</span>
+              </p>
+            )}
+          </div>
+        </div>
+      </CardContent>
+    </CardWithShadow>
+  );
+};
+
+const ProgressRatio = memo(Component);
+export { ProgressRatio };
