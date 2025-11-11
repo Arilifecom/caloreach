1cec80de1c5e383c4e19dd4d50bafff74c79465c
Ari <arilife.com@gmail.com>
2025-11-11 23:56:26 +0900

    Create mealRecord history child page

---
+++ src/app/dashboard/histories/[date]/page.tsx
@@ -0,0 +1,40 @@
+import {
+  MealRecordSection,
+  ProgressSection,
+} from "@/app/dashboard/_components";
+import { PageHeader } from "@/components";
+import { fetchUserDailyMealRecords } from "@/utils/api/mealRecords";
+import { getUser } from "@/utils/auth";
+import { formatDateWithDay } from "@/utils/format";
+import { getQueryClient, mealRecordkeys } from "@/utils/tanstack";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+export default async function MealDetailPage({
+  params,
+}: {
+  params: { date: string };
+}) {
+  const userId = await getUser();
+  const queryClient = getQueryClient();
+  const { date } = await params;
+
+  await queryClient.prefetchQuery({
+    queryKey: mealRecordkeys.dailyList(userId, date),
+    queryFn: () => fetchUserDailyMealRecords(userId, date),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <>
+      <PageHeader
+        title={formatDateWithDay(new Date(date))}
+        description="過去の食事履歴詳細です"
+      />
+      <HydrationBoundary state={dehydratedState}>
+        <ProgressSection userId={userId} date={date} />
+        <MealRecordSection userId={userId} date={date} />
+      </HydrationBoundary>
+    </>
+  );
+}
