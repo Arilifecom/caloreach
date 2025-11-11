05c76a528e0f7069771ac493ed94d38def73040f
Ari <arilife.com@gmail.com>
2025-11-11 18:01:00 +0900

    Create History page UI

---
+++ src/app/dashboard/histories/page.tsx
@@ -0,0 +1,25 @@
+import { HistoryList } from "@/app/dashboard/histories/_components";
+import { PageHeader } from "@/components";
+import { getDailyKcalSummary } from "@/utils/api/history";
+import { checkAuth, getUser } from "@/utils/auth";
+
+export default async function RegularFoodsPage() {
+  await checkAuth();
+  const userId = await getUser();
+
+  const initialData = await getDailyKcalSummary({
+    userId: userId,
+    offset: 0,
+    limit: 7,
+  });
+
+  return (
+    <>
+      <PageHeader
+        title="過去の食事履歴"
+        description="日付ごとに摂取したカロリー合計です。"
+      />
+      <HistoryList initialData={initialData} userId={userId} />
+    </>
+  );
+}
