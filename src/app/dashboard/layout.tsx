ab5d6d318b32d58b5b89f319845890cfea72188a
Ari <arilife.com@gmail.com>
2025-11-12 12:02:54 +0900

    Create dashboard nav and Header nav

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { DashboardNav } from "@/app/dashboard/_components";
+import { DashboardHeader, DashboardNav } from "@/app/dashboard/_components";
 import TanstackQueryProvider from "@/app/dashboard/provider";
 import { Toaster } from "@/components/ui";
 
@@ -6,12 +6,15 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="relative font-sans grid grid-rows-[20px_1fr_20px] mx-auto justify-items-center min-h-screen max-w-md text-sm p-6 pb-20">
-      <main className="flex flex-col gap-[32px] w-full row-start-2 items-center">
-        <Toaster />
-        <TanstackQueryProvider>{children}</TanstackQueryProvider>
-        <DashboardNav />
-      </main>
-    </div>
+    <>
+      <DashboardHeader />
+      <div className="relative font-sans grid grid-rows-[20px_1fr_20px] mx-auto justify-items-center min-h-screen max-w-md text-sm p-6 pb-20">
+        <main className="flex flex-col gap-[32px] w-full row-start-2 items-center">
+          <Toaster />
+          <TanstackQueryProvider>{children}</TanstackQueryProvider>
+          <DashboardNav />
+        </main>
+      </div>
+    </>
   );
 }
