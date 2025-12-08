b501c7342fb3d0f432eeb54c6de9f794a8dd752b
Ari <arilife.com@gmail.com>
2025-12-08 22:23:42 +0900

    Create incremetal search hooks

---
+++ src/app/dashboard/_hooks/useFoodSearch.ts
@@ -0,0 +1,26 @@
+"use client";
+
+import { useDebounce } from "@/app/dashboard/_hooks/";
+import { fetchFoodsBySearch } from "@/utils/api/mealRecords";
+import { foodskeys, TErrCodes } from "@/utils/tanstack";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+export const useFoodSearch = () => {
+  const [query, setQuery] = useState("");
+  const debounced = useDebounce(query, 400);
+
+  //incremental search
+  const searchResult = useQuery({
+    queryKey: foodskeys.list(debounced),
+    queryFn: () => fetchFoodsBySearch(debounced),
+    enabled: debounced !== "",
+    meta: { errCode: TErrCodes.FOOD_SEARCH_FAILED },
+  });
+
+  return {
+    query,
+    setQuery,
+    searchResult,
+  };
+};
