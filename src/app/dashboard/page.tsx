0745e2ba9e8af4ad47aab00b4a560677c2949564
Ari <arilife.com@gmail.com>
2025-12-09 11:56:15 +0900

    Remove prefetchQuery to optimize loading for the main page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,37 +3,24 @@ import {
   ProgressSection,
 } from "@/app/dashboard/_components";
 import { PageHeader } from "@/components";
-import { fetchUserDailyMealRecords } from "@/utils/api/mealRecords";
 import { getUserId } from "@/utils/auth";
-import { formatDateWithDay, getToday } from "@/utils/format/date";
-import { getQueryClient, mealRecordkeys } from "@/utils/tanstack";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { formatDateWithDay, formatYYMMDD } from "@/utils/format/date";
 
 export default async function Dashboard() {
   const userId = await getUserId();
-  const queryClient = getQueryClient();
 
-  const today = getToday();
-  const displayDate = new Date();
-  const formatedDisplayDate = formatDateWithDay(displayDate);
-
-  await queryClient.prefetchQuery({
-    queryKey: mealRecordkeys.dailyList(userId, today),
-    queryFn: () => fetchUserDailyMealRecords(userId, today),
-  });
-
-  const dehydratedState = dehydrate(queryClient);
+  const today = new Date();
+  const date = formatYYMMDD(today);
+  const formatedDisplayDate = formatDateWithDay(today);
 
   return (
     <>
       <PageHeader
         title={`今日の食事記録｜ ${formatedDisplayDate} `}
-        description={`目標達成までのカロリー進捗を確認しましょう。今日の一歩が習慣になります。`}
+        description="目標達成までのカロリー進捗を確認しましょう。今日の一歩が習慣になります。"
       />
-      <HydrationBoundary state={dehydratedState}>
-        <ProgressSection userId={userId} date={today} />
-        <MealRecordSection userId={userId} date={today} />
-      </HydrationBoundary>
+      <ProgressSection userId={userId} date={date} />
+      <MealRecordSection userId={userId} date={date} />
     </>
   );
 }
