8999c0bf2b19abb4a4377f4192cea4bd49a1a2a6
Ari <arilife.com@gmail.com>
2025-12-17 15:33:31 +0900

    Add Suspense and loading skeletons to History page

---
+++ src/app/dashboard/histories/_components/HistoryListSkeltone.tsx
@@ -0,0 +1,11 @@
+import { HistoryListItemSkeltone } from "@/app/dashboard/histories/_components/HistoryListItemSkeltone";
+
+export const HistoryListSkeltone = () => {
+  return (
+    <ul className="w-full">
+      <HistoryListItemSkeltone />
+      <HistoryListItemSkeltone />
+      <HistoryListItemSkeltone />
+    </ul>
+  );
+};
