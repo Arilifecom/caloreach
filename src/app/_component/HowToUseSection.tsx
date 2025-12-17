18964b82c447fa6192a2242c4b8406fb3f8cda4c
Ari <arilife.com@gmail.com>
2025-12-17 18:00:08 +0900

    Fix Home video playback and adjust Hero image size

--- src/app/_component/HowToUseSection.tsx
+++ src/app/_component/HowToUseSection.tsx
@@ -1,57 +1,52 @@
-import { PageHeader } from "@/components";
-
 const steps = [
   {
     title: "食事を記録（検索 or 手入力）",
     description:
-      "食品検索で量を入力するだけでカロリーを自動計算。その日の食事をすぐに記録できます。",
+      "食品検索で量を入力するだけでカロリーを自動計算。\nその日の食事をすぐに記録できます。",
     step: 1,
     video: (
       <video
         src="/videos/step-1.mp4"
-        width="250px"
-        height="auto"
         controls
         playsInline
-        preload="none"
-        muted
+        preload="metadata"
+        muted={true}
         poster="/images/sutep1-thumbnail.png"
+        className="w-full h-full object-contain -webkit-playsinline"
       />
     ),
   },
   {
     title: "レギュラーフードを登録",
     description:
-      "よく食べるメニューを事前に保存。次回からはワンタップで素早く記録できます。",
+      "よく食べるメニューを事前に保存。\n次回からはワンタップで素早く記録できます。",
     step: 2,
     video: (
       <video
         src="/videos/step-2.mp4"
-        width="250px"
-        height="auto"
         controls
         playsInline
-        preload="none"
-        muted
+        preload="metadata"
+        muted={true}
         poster="/images/sutep2-thumbnail.png"
+        className="w-full h-full object-contain -webkit-playsinline"
       />
     ),
   },
   {
     title: "履歴で振り返る",
     description:
-      "日ごとの合計カロリーを一覧で確認。記録漏れがあっても、詳細ページで食事を追加・編集できます。",
+      "日ごとの合計カロリーを一覧で確認\n記録漏れがあっても、詳細ページで食事を追加・編集できます。",
     step: 3,
     video: (
       <video
         src="/videos/step-3.mp4"
-        width="250px"
-        height="auto"
         controls
         playsInline
-        preload="none"
-        muted
+        preload="metadata"
+        muted={true}
         poster="/images/sutep3-thumbnail.png"
+        className="w-full h-full object-contain -webkit-playsinline"
       />
     ),
   },
@@ -65,23 +60,36 @@ export const HowToUseSection = () => {
         <span className="text-2xl ml-2">-カロリーチの使い方-</span>
       </div>
 
-      <ul className="flex flex-col my-8 gap-8 md:gap-20">
-        {steps.map((item, index) => (
-          <li key={index}>
-            <div className="flex flex-col items-center text-center gap-4 py-4">
+      <ul className="flex flex-col my-8 gap-10 md:gap-56">
+        {steps.map((content, index) => (
+          <li
+            key={index}
+            className="flex flex-col items-center md:flex-row-reverse"
+          >
+            <div className="flex flex-col items-center text-center gap-4 mb-6 md:w-1/2">
               <div className="flex flex-col items-center">
                 <p className="text-yellow-400 text-lg font-black">STEP</p>
                 <div className="flex h-10 w-10 items-center justify-center rounded-full bg-primary">
                   <span className="text-white text-lg font-black">
-                    {item.step}
+                    {content.step}
                   </span>
                 </div>
               </div>
 
-              <PageHeader title={item.title} description={item.description} />
+              <div className="max-w-4xl">
+                <h3 className="text-xl font-bold mb-4">{content.title}</h3>
+                <p
+                  style={{ whiteSpace: "pre-line" }}
+                  className="text-left max-w-72 text-gray-700 font-medium md:text-center md:max-w-full"
+                >
+                  {content.description}
+                </p>
+              </div>
             </div>
 
-            <div className="flex justify-center">{item.video}</div>
+            <div className="flex justify-center w-[250px] aspect-[9/16] mx-auto md:w-[300px]">
+              {content.video}
+            </div>
           </li>
         ))}
       </ul>
