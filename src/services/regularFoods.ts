78a11f0dfbcea0342c44797f5a557c02d7e26a64
Ari <arilife.com@gmail.com>
2026-02-13 10:28:05 +0900

    Replace client functions with Hono RPC

---
+++ src/services/regularFoods.ts
@@ -0,0 +1,41 @@
+import { createClientRPC } from "@/lib/createClientRPC";
+import { RegularFoodsRequest } from "@/shared/types";
+
+//fetch
+export const fetchRegularFoods = async () => {
+  const client = await createClientRPC();
+  const res = await client.api.dashboard.regularfoods.$get();
+  const data = await res.json();
+  return data.mealRecords;
+};
+
+//Insert
+export const addRegularFood = async (InputData: RegularFoodsRequest) => {
+  const client = await createClientRPC();
+  const res = await client.api.dashboard.regularfoods.$post({
+    json: InputData,
+  });
+
+  const { data } = await res.json();
+  return data;
+};
+
+//Delete
+export const deleteRegularFood = async (InputData: RegularFoodsRequest) => {
+  const client = await createClientRPC();
+  await client.api.dashboard.regularfoods[":id"].$delete({
+    param: { id: InputData.id },
+  });
+};
+
+//Update
+export const editRegularFood = async (InputData: RegularFoodsRequest) => {
+  const client = await createClientRPC();
+  const res = await client.api.dashboard.regularfoods[":id"].$put({
+    param: { id: InputData.id },
+    json: InputData,
+  });
+
+  const { data } = await res.json();
+  return data;
+};
