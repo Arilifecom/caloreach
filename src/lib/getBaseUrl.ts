b627f7509308e574c269de09619a3ae03b3661fe
Ari <arilife.com@gmail.com>
2026-02-17 12:50:58 +0900

    Add URL for preview environment

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,7 +1,12 @@
 export function getBaseUrl() {
+  if (process.env.VERCEL_ENV === "preview") {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+
   if (process.env.NODE_ENV === "production") {
     return process.env.NEXT_PUBLIC_ORIGIN!;
   }
+
   return typeof window !== "undefined"
     ? window.location.origin
     : process.env.NEXT_PUBLIC_ORIGIN!;
