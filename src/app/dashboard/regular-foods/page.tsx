13b4bcc7ab1431816eaf57aff1b615736555a5fb
Ari <arilife.com@gmail.com>
2026-02-20 12:52:27 +0900

    Add prefetchQuery

--- src/app/dashboard/regular-foods/page.tsx
+++ src/app/dashboard/regular-foods/page.tsx
@@ -1,11 +1,25 @@
 import { RegularFoodSection } from "@/app/dashboard/regular-foods/_components";
+import { getQueryClient, RegularFoodskeys } from "@/lib/tanstack";
+import { fetchRegularFoodsServer } from "@/services/regularFoods";
 import { getUserId } from "@/utils/db/auth";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 export default async function RegularFoodsPage() {
   const userId = await getUserId();
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: RegularFoodskeys.list(userId),
+    queryFn: async () => await fetchRegularFoodsServer(),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <>
-      <RegularFoodSection userId={userId} />
+      <HydrationBoundary state={dehydratedState}>
+        <RegularFoodSection userId={userId} />
+      </HydrationBoundary>
     </>
   );
 }
